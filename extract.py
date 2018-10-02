a9b1f6bb749ce4985172c3c33dd8bf987a9b68a5
kanishkagarg <41885061+kanishkagarg@users.noreply.github.com>
2018-10-02 14:45:12 +0530

    Add files via upload

---
+++ extract.py
@@ -0,0 +1,28 @@
+import csv 
+  
+filename = "data.csv"
+fields = [] 
+rows = [] 
+  
+with open(filename, 'r') as csvfile: 
+    
+    csvreader = csv.reader(csvfile) 
+      
+    fields = csvreader.next() 
+  
+    
+    for row in csvreader: 
+        rows.append(row) 
+  
+    
+    print("Total no. of rows: %d"%(csvreader.line_num)) 
+  
+
+print('Field names are:' + ', '.join(field for field in fields)) 
+  
+ 
+print('\nFirst 5 rows are:\n') 
+for row in rows[:5]: 
+    for col in row: 
+        print("%10s"%col), 
+    print('\n') 
